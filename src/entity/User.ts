71e441a31206efd4630d7d2a375d0e3c078b00ba
mihirjayavant <mihir.jayavant@accionlabs.com>
2019-08-28 16:24:37 +0530

    get all users working

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,55 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
-
-@Entity()
-export class User {
-
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    Unique,
+    CreateDateColumn,
+    UpdateDateColumn
+  } from "typeorm";
+  import { Length, IsNotEmpty } from "class-validator";
+  import * as bcrypt from "bcryptjs";
+import { IUser } from "../models";
+  
+  @Entity()
+  @Unique(["username"])
+  export class User implements IUser {
+      
     @PrimaryGeneratedColumn()
     id: number;
+  
+    @Column()
+    @Length(4, 20)
+    username: string;
 
     @Column()
-    firstName: string;
+    @Length(2, 100)
+    firstname: string;
 
     @Column()
-    lastName: string;
-
+    @Length(2, 100)
+    lastname: string;
+  
     @Column()
-    age: number;
-
-}
+    @Length(4, 100)
+    password: string;
+  
+    @Column()
+    @IsNotEmpty()
+    role: string;
+  
+    @Column()
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @Column()
+    @UpdateDateColumn()
+    updatedAt: Date;
+  
+    hashPassword() {
+      this.password = bcrypt.hashSync(this.password, 8);
+    }
+  
+    checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+      return bcrypt.compareSync(unencryptedPassword, this.password);
+    }
+  }
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import {Request, Response} from "express";
 import * as helmet from "helmet";
 import * as cors from "cors";
-import {Routes} from "./routes";
-import {User} from "./entity/User";
+import routes from "./routes";
 
 createConnection().then(async connection => {
 
